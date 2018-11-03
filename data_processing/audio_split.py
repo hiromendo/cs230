a8516764adc64bef60b78a0ad5eb6ae3d105bd03
hiromendo <hiromendo@gmail.com>
2018-11-03 15:08:22 -0700

    audio split

---
+++ data_processing/audio_split.py
@@ -0,0 +1,22 @@
+from pydub import AudioSegment
+
+
+
+
+def audio_split(audioinput, time):
+
+	t1 = 0 * 1000 #Works in milliseconds
+	t2 = time * 1000
+	
+	newAudio = AudioSegment.from_wav(audioinput)
+	newAudio = newAudio[t1:t2]
+
+
+	newAudio.export('sx82_short2.wav', format="wav")
+
+
+
+
+audio = "sx82.wav"
+
+audio_split(audio, 2)
