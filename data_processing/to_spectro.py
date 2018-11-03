5307483173ee6633d0350f0ec45bb3a6d9c97aac
Xi Hua <xihua@Xis-MacBook-Pro-2.local>
2018-11-03 14:32:24 -0700

    mod spectro

--- data_processing/to_spectro.py
+++ data_processing/to_spectro.py
@@ -3,6 +3,7 @@ from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
 
+#read in audio file
 file_name = "goo"
 sample_rate, audio = wavfile.read(file_name + '.wav')
 
@@ -18,20 +19,15 @@ def log_specgram(audio, sample_rate, window_size=20,
                                     detrend=False)
     return freqs, times, np.log(spec.T.astype(np.float32) + eps)
 
+#set up plt
 fig = plt.figure(frameon=False)
+#suppress axis
 ax = plt.Axes(fig, [0., 0., 1., 1.])
 ax.set_axis_off()
 fig.add_axes(ax)
+#extract spectrogram
 _,_, spectrogram = log_specgram(audio, sample_rate)
+#plot spectrogram
 plt.imshow(spectrogram.T, aspect='auto', origin='lower')
+#save as image file
 fig.savefig(file_name + "spectro2.png")
-'''
-sample_rate, samples = wavfile.read('path-to-mono-audio-file.wav')
-frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
-
-plt.pcolormesh(times, frequencies, spectrogram)
-plt.imshow(spectrogram)
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
-plt.show()
-'''
