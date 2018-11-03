a9e57a51042d09719036f0463a88a6f009aff02f
Xi Hua <xihua@Xis-MacBook-Pro-2.local>
2018-11-03 15:10:42 -0700

    updated

--- data_processing/to_spectro.py
+++ data_processing/to_spectro.py
@@ -3,10 +3,6 @@ from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
 
-#read in audio file
-file_name = "goo"
-sample_rate, audio = wavfile.read(file_name + '.wav')
-
 def log_specgram(audio, sample_rate, window_size=20,
                  step_size=5, eps=1e-10):
     nperseg = int(round(window_size * sample_rate / 1e3))
@@ -19,16 +15,36 @@ def log_specgram(audio, sample_rate, window_size=20,
                                     detrend=False)
     return freqs, times, np.log(spec.T.astype(np.float32) + eps)
 
-#set up plt
-fig = plt.figure(frameon=False)
-#suppress axis
-ax = plt.Axes(fig, [0., 0., 1., 1.])
-ax.set_axis_off()
-fig.add_axes(ax)
-#extract spectrogram
-_,_, spectrogram = log_specgram(audio, sample_rate)
-print(np.shape(spectrogram))
-#plot spectrogram
-plt.imshow(spectrogram.T, aspect='auto', origin='lower')
-#save as image file
-fig.savefig(file_name + "spectro2.png")
+#Saves a spectogram as an image
+def save_spectro_as_img(spectrogram, file_name): #takes a spectrogram array
+    #set up plt
+    fig = plt.figure(frameon=False)
+    #suppress axis
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    #plot spectrogram
+    plt.imshow(spectrogram.T, aspect='auto', origin='lower')
+    #save as image file
+    fig.savefig(file_name + "spectro.png")
+
+
+if __name__ == '__main__':
+    #initialize variables
+    trainX = []
+    #put spectrogram into array
+    for file in file_names:
+        #read in audio file
+        sample_rate, audio = wavfile.read(file + '.wav')
+        #extract spectrogram
+        _,_, spectrogram = log_specgram(audio, sample_rate)
+        print(np.shape(spectrogram))
+        #add assert size line when we know size here
+        spectro_flat = spectrogram.reshape([])
+        trainX.append(spectrogram)
+    np.save("trainX",trainX)
+
+
+
+
+
